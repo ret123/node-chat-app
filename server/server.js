9f94ec44d21ec3bd531da7ba320679811af82c75
rathish nair <rnair@localhost.localdomain>
2018-03-29 17:46:05 +0530

    Styling done for chat app

--- server/server.js
+++ server/server.js
@@ -16,9 +16,10 @@ io.on('connection', (socket) => {
     socket.emit('newMessage', generateMessage('Admin','Welcome to the chat app'));
     socket.broadcast.emit('newMessage',generateMessage('Admin','New user joined'));
       
-    socket.on('createMessage', (message) => {
+    socket.on('createMessage', (message, callback) => {
         console.log(message);
         io.emit('newMessage',generateMessage(message.from,message.text));
+        callback();
     });
     socket.on('createLocationMessage', (coords) => {
         io.emit('newLocationMessage', generateLocationMessage('Admin',coords.latitude,coords.longitude));
