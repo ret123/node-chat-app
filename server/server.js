d0f85e8a8d7415b234707b8c8eb7ccec9fb7a223
rathish nair <rnair@localhost.localdomain>
2018-03-29 11:37:09 +0530

    Greet new user and alert othehers

--- server/server.js
+++ server/server.js
@@ -13,6 +13,17 @@ app.use(express.static(publicPath));
 
 io.on('connection', (socket) => {
     console.log("New User connected");
+    socket.emit('newMessage',  {
+        from  : 'Admin',
+        text: 'Welcome to the chat app',
+        createdAt: new Date().getTime()
+    });
+    socket.broadcast.emit('newMessage', {
+        from: 'Admin',
+        text: 'New user joined',
+        createdAt: new Date().getTime()
+    });
+    socket.broadcast.emit('')
     socket.on('createMessage', (message) => {
         console.log(message);
         io.emit('newMessage',  {
@@ -20,6 +31,11 @@ io.on('connection', (socket) => {
         text: message.text,
         createdAt: new Date().getTime() 
         });
+        // socket.broadcast.emit('newMessage', {
+        //     from: message.from,
+        //     text: message.text,
+        //     createdAt: new Date().getTime() 
+        // });
     });
     socket.on('disconnect', () => {
         console.log('User Disconnected');
