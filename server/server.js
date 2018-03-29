4dfadac6ceafa52a7701467eaa21c0ee4c29933c
rathish nair <rnair@localhost.localdomain>
2018-03-29 12:59:47 +0530

    Create generateMessage utility

--- server/server.js
+++ server/server.js
@@ -3,8 +3,8 @@ const http = require('http');
 const express = require('express');
 const socketIO = require('socket.io');
 
+const {generateMessage} = require('./utils/message');
 const publicPath = path.join(__dirname,'../public');
-
 const port = process.env.PORT || 3000;
 const app =express();
 var server = http.createServer(app);
@@ -13,24 +13,12 @@ app.use(express.static(publicPath));
 
 io.on('connection', (socket) => {
     console.log("New User connected");
-    socket.emit('newMessage',  {
-        from  : 'Admin',
-        text: 'Welcome to the chat app',
-        createdAt: new Date().getTime()
-    });
-    socket.broadcast.emit('newMessage', {
-        from: 'Admin',
-        text: 'New user joined',
-        createdAt: new Date().getTime()
-    });
-    socket.broadcast.emit('')
+    socket.emit('newMessage', generateMessage('Admin','Welcome to the chat app'));
+    socket.broadcast.emit('newMessage',generateMessage('Admin','New user joined'));
+      
     socket.on('createMessage', (message) => {
         console.log(message);
-        io.emit('newMessage',  {
-        from: message.from,
-        text: message.text,
-        createdAt: new Date().getTime() 
-        });
+        io.emit('newMessage',generateMessage(message.from,message.text));
         // socket.broadcast.emit('newMessage', {
         //     from: message.from,
         //     text: message.text,
